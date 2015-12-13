f0f580123531f2279d2e503431c8315e2359eea3
Pradeep Kumar Singh <stud.pradeep@gmail.com>
2015-12-13 12:38:52 +0530

    ADD POST TODO

--- server.js
+++ server.js
@@ -1,68 +1,71 @@
-var express = require('express');
-var app = express();
-
-var PORT = process.env.PORT || 3000 ;
-
-var todos = [{
-	
-	id : 1,
-	description : " Meet mom for lunch",
-	completed  : false
-	
-},{
-	
-	id : 2,
-	description : "Go to market",
-	completed : false
-},{
-	
-	id :3,
-	description : " Rock the world !!",
-	completed : true
-}];
-
-//GET Todo/
-
-app.get('/' ,function(req,res) {
-	
-	res.send('TODO API ROOT..');
-	
-});
-
-app.get('/todos',function(req,res){
-	
-	res.json(todos);
-
-});
-
-app.get('/todos/:id',function(req,res){
-	
-	var todoId= parseInt(req.params.id,10);
-	var matchtodo ;
-	
-	
-	todos.forEach(function(num)
-	{
-		if( todoId === num.id )
-				{
-			matchtodo = num;
-			console.log('Required Information..'+ num);
-		}
-	});
-	
-	if(matchtodo)
-	{
-		res.json(matchtodo);
-	}
-	else{
-		res.status(404).send();
+		var express = require('express');
+		var app = express();
 		
-	}
-	
-
-});
-
-app.listen(PORT, function(){
-	
-	console.log("Express Listening on PORT" + PORT);
-});
+		var bodyParser = require('body-parser');
+		
+		var PORT = process.env.PORT || 3000 ;
+		
+		var todos = [];
+		var todoNextId =1;
+		
+		app.use(bodyParser.json());
+		
+		//GET Todo/
+		
+		app.get('/' ,function(req,res) {
+		
+		res.send('TODO API ROOT..');
+		
+		});
+		
+		app.get('/todos',function(req,res){
+		
+		res.json(todos);
+		
+		});
+		
+		//GET Todo ID
+		
+		app.get('/todos/:id',function(req,res){
+		
+		var todoId= parseInt(req.params.id,10);
+		var matchtodo ;
+		
+		
+		todos.forEach(function(num)
+		{
+			if( todoId === num.id )
+					{
+				matchtodo = num;
+				console.log('Required Information..'+ num);
+			}
+		});
+		
+		if(matchtodo)
+		{
+			res.json(matchtodo);
+		}
+		else{
+			res.status(404).send();
+		
+		}	
+		});
+		
+		//POST /todos
+		app.post('/todos', function(req,res)
+		{
+			var body =req.body;
+			body.id = todoNextId++;
+			
+			todos.push(body);
+			
+			//console.log('description..'+ body.description);
+			res.json(body);
+			console.log('and ID is..'+ todoNextId);
+			
+		});
+		
+		app.listen(PORT, function(){
+		
+		console.log("Express Listening on PORT" + PORT);
+		});
